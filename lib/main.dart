0cb2e84d1667d213670febec2aafc0823eee3ada
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-14 18:12:15 -0600

    Creacion del app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/views/chat/chat_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 8).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Material App Bar"),
-        ),
-        body:  Center(
-          child: FilledButton.tonal(onPressed: (){}, 
-          child: Text("Click me"))
-        ),
-      )
+      //Perzonalizar los colores de app
+      theme: AppTheme(selectedColor: 4).theme(),
+      home: const ChatScreen()
     );
   }
 }
